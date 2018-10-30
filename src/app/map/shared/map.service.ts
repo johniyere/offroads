afeb7c96781918d0ca3efd7b5026b37548af3db7
John Iyere <john.iyere@student.manchester.ac.uk>
2018-10-30 19:37:39 +0000

    Register mapbox token in map service

--- src/app/map/shared/map.service.ts
+++ src/app/map/shared/map.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
 
-  constructor() { }
+  constructor() {
+    (mapboxgl as any).accessToken = environment.mapbox.accessToken;
+   }
 }
