1687431d10551effcab7f2b388808de8d489ea37
John Iyere <john.iyere@student.manchester.ac.uk>
2018-10-30 19:23:59 +0000

    Added mapbox default public key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken: 'pk.eyJ1Ijoiam9obml5ZXJlIiwiYSI6ImNqbXVxaHNtOTJxenUza29lZDE3MGlidncifQ.W-M8wzC7mnnXvH47GxVN4w'
+  }
 };
