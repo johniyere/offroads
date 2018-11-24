ff754083d4df3404eac3e22ebd83b2e43302b6e3
John Iyere <john.iyere@student.manchester.ac.uk>
2018-11-24 12:12:59 +0000

    Request point data from mapbox terrain api

--- src/app/map/shared/map.service.ts
+++ src/app/map/shared/map.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
+import { LngLat } from 'mapbox-gl';
 import * as mapboxgl from 'mapbox-gl';
 import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
 
-  constructor() {
+  constructor(private http: HttpClient) {
     (mapboxgl as any).accessToken = environment.mapbox.accessToken;
-   }
+  }
+
+  getMapTerrainData(coordinates: LngLat) {
+    const coordString = `${coordinates.lng},${coordinates.lat}`;
+    console.log(coordString);
+    const endpoint = `https://api.mapbox.com/v4/mapbox.mapbox-terrain-v2/tilequery/
+    ${coordString}.json?access_token=${environment.mapbox.accessToken}`;
+    return this.http.get(endpoint);
+  }
 }
