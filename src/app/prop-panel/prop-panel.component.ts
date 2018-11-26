68e160fc21d76417b176f0e001cb0d491a8556d4
John Iyere <john.iyere@student.manchester.ac.uk>
2018-11-26 21:34:58 +0000

    Added chart.js dependencies Plot simple area graph

--- src/app/prop-panel/prop-panel.component.ts
+++ src/app/prop-panel/prop-panel.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Chart } from 'chart.js';
 @Component({
   selector: 'ofr-prop-panel',
   templateUrl: './prop-panel.component.html',
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropPanelComponent implements OnInit {
 
+  chart: Chart;
   constructor() { }
 
   ngOnInit() {
+
+    this.chart = new Chart('myChart', {
+      type: 'line',
+      data: {
+        labels: ['0.2', '0.4', '0.6', '0.7', '1.0', '1.2'],
+        datasets: [
+          {
+            data: [100, 110, 150, 160, 155, 135],
+            borderColor: '#3cba9f',
+            fill: 'origin'
+          },
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }]
+        }
+      }
+    });
+
   }
 
 }
